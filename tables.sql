95170a0463cd8e090cb15d00f124bb80d308a1fe
orlando <orlandolozaarr@gmail.com>
2020-01-19 15:30:30 -0600

    tables

--- tables.sql
+++ tables.sql
@@ -1,16 +1,20 @@
 -- docker run mysql db container: sudo docker run -v cubingmtydb:/var/lib/mysql --name cubingmtymysql -d -p 3306:3306 --network cubingmtynet -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=cubingmty mysql:5.7.29 --
 
+-- docker run cubingmty app: sudo docker image build -t cubing-mty . --
+
 
 CREATE TABLE `cm_times` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Cm_User_Id` int(11) NOT NULL,
-  `time` int Not  NULL,
+  `time` int(11) NOT NULL,
   `scramble` varchar(100) NOT NULL,
-  `DNF` char DEFAULT 'N' NOT NULL,
-  `Penalties` integer DEFAULT 0 NOT NULL,
+  `DNF` char(1) NOT NULL DEFAULT 'N',
+  `Penalties` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`Id`),
+  KEY `cm_time_ibfk_1` (`Cm_User_Id`),
   CONSTRAINT `cm_time_ibfk_1` FOREIGN KEY (`Cm_User_Id`) REFERENCES `cm_user` (`Id`)
-) 
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `cm_user` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
@@ -18,15 +22,27 @@ CREATE TABLE `cm_user` (
   `WCAID` varchar(150) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(100) NOT NULL,
-  `Enabled` TINYINT(1) DEFAULT 1 NOT NULL,
-  UNIQUE KEY `cm_user_idx_1` (`Email`),
-  PRIMARY KEY (`Id`)
-) 
+  `Enabled` tinyint(1) NOT NULL DEFAULT '1',
+  `Img` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `cm_user_idx_1` (`Email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 CREATE TABLE `cm_roles` (
-	`Email` varchar(50) NOT NULL,
-	`Role` varchar(50) NOT NULL,
-	UNIQUE KEY `cm_roles_idx_1` (`email`,`role`),
-	CONSTRAINT `cm_roles_ibfk_1`
-	FOREIGN KEY (`Email`) REFERENCES `cm_user` (`Email`)
-) 
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Role` varchar(50) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cm_user_roles` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Id_User` int(11) NOT NULL,
+  `Id_Role` int(11) NOT NULL,
+  PRIMARY KEY (`Id`),
+  KEY `cm_user_roles_ibfk_1` (`Id_User`),
+  KEY `cm_user_roles_ibfk_2` (`Id_Role`),
+  CONSTRAINT `cm_user_roles_ibfk_1` FOREIGN KEY (`Id_User`) REFERENCES `cm_user` (`Id`),
+  CONSTRAINT `cm_user_roles_ibfk_2` FOREIGN KEY (`Id_Role`) REFERENCES `cm_roles` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
